45b273497ef5fd8da91d64fdabe23aac135dda5d
Tal Barmocha <Talb999@gmail.com>
2024-08-12 02:11:20 +0300

    #

--- ball.asm
+++ ball.asm
@@ -5,24 +5,58 @@
 ;Graphics:
 INCLUDE fontdata.asm
 INCLUDE colors.asm
+
 ;INCLUDE manu.asm
 location_x DW 05d
 location_y DW 05d
-background_color DB 100d
+background_color equ 100d
 ;Balls:
 color_picker DW 0d
 current_ball DB 0d
 next_ball DB 0d
 seed DW 1d
-;ghghg
 ;Game:
 score DW 0d  ; MAX score is ≈ 64K -> limit max score to 50K
 lifes DB 5d
+init_player_x equ 119d
+init_player_y equ 180d
+player_x DW init_player_x
+player_y Dw init_player_y
+mouse_x DW 0d
+mouse_y DW 0d
 
 .code
-GAME_START proc
+include mouse.asm
+main proc
     mov ax, @data               ; Load data segment address into AX
     mov ds, ax                  ; Move data segment address to DS
+    call IVT_change
+    call GAME_START
+    main_loop:
+    ; Check if a key is pressed
+    ;call check_mouse
+    mov ah, 1          ; Function 01h: Check for keystroke availability
+    int 16h            ; BIOS keyboard interrupt
+    jz main_loop       ; If no key is pressed, continue looping
+
+    ; If a key is pressed, get the key
+    mov ah, 0          ; Function 00h: Get keystroke
+    int 16h            ; BIOS keyboard interrupt
+
+    cmp al, 'q'        ; Compare the pressed key with 'q'
+    jne main_loop      ; If not 'q', continue looping
+    ;exit
+    ;move back to taxt mode
+    call IVT_return
+    mov ah, 00h
+    mov al, 03h
+    int 10h
+    mov ah, 4Ch                ; Terminate the program
+    int 21h                    ; DOS interrupt to exit
+main endp 
+
+
+GAME_START proc
     ;set graphic mode
     mov ah, 00h
     mov al, 13h
@@ -39,12 +73,11 @@ GAME_START proc
         call draw_balls_line
         add location_y,12d
     loop game_lines
-
     ;draw player, lives and next ball
     call get_currBall_nxtBall
     ;Draw Player
-    mov location_x, 119d
-    mov location_y, 180d
+    mov location_x, init_player_x
+    mov location_y, init_player_y
     mov bl,current_ball
     call draw_ball
     ;Draw Next Ball
@@ -61,17 +94,7 @@ GAME_START proc
         call draw_ball
         add location_x,14d
     loop print_lifes
-    
-    ;wait for key press to continue 
-    mov ah, 00h
-    int 16h
-    ;exit
-    ;move back to taxt mode
-    mov ah, 00h
-    mov al, 03h
-    int 10h
-    mov ah, 4Ch                ; Terminate the program
-    int 21h                    ; DOS interrupt to exit
+    ret
 GAME_START endp
 
 
@@ -411,4 +434,4 @@ get_currBall_nxtBall proc uses ax bx dx si
     ret
 get_currBall_nxtBall endp
 
-end GAME_START
+end main
--- mouse.asm
+++ mouse.asm
@@ -1,102 +1,229 @@
-check_mouse_and_shoot proc
-    push ax
-    push bx
-    push cx
-    push dx
-    push si
-    push di
-
+;==================================================
+;This procedure handles the mouse interrupt
+;==================================================
+check_mouse proc uses ax bx cx dx
     ; Check for mouse click
     mov ax, 3   ; Get mouse status
     int 33h
-    test bx, 2  ; Check right button
+    test bx, 1  ; Check left button
     jz no_click
-
     ; Get mouse position
     mov ax, 3
     int 33h     ; Automatically enters coordinates for DX and CX
     shr cx, 1   ; Divide CX by 2 to convert from 640x200 to 320x200
-
+    click:
+    test bx, 0 ;check if the button is lifted
+    jne click
     ; Store mouse position
-    mov [mouse_x], cx
-    mov [mouse_y], dx
+    mov mouse_x, cx
+    mov mouse_y, dx
+    call shoot
+    no_click:
+    ret
+check_mouse endp
 
-    ; Set initial ball position
-    mov [ball_x], 119d
-    mov [ball_y], 180d
 
+shoot proc uses ax bx cx dx si di   
+    ; Calculate direction
+    mov ax, mouse_x
+    sub ax, player_x   
+    mov bx, player_y
+    sub bx, mouse_y
+    ; Normalize direction (simple approach)
+    cmp ax, 0
+    jge shoot_continue
+    neg ax          ;In case the difference is negative
+    shoot_continue:
+    add ax, bx
+    mov cx, ax  ; CX = |dx| + |dy|
+    ;SI = x step
+    mov ax, mouse_x
+    sub ax, player_x
+    mov dx, 2
+    imul dx
+    idiv cx
+    mov si, ax
+    ;DI = y step
+    mov ax, mouse_y
+    sub ax, player_y
+    mov dx, 2
+    imul dx  ; Adjust speed here
+    idiv cx
+    mov di, ax
     shoot_loop:
-        ; Calculate direction
-        mov ax, [mouse_x]
-        sub ax, [ball_x]    
-        mov bx, [mouse_y]
-        sub bx, [ball_y]
-
-        ; Normalize direction (simple approach)
-        cmp ax, 0
-        jge check_y
-        neg ax          ;In case the difference is negative
-    check_y:
-        cmp bx, 0
-        jge continue
-        neg bx
-    continue:
-        add ax, bx
-        mov cx, ax  ; CX = |dx| + |dy|
-
-        mov ax, [mouse_x]
-        sub ax, [ball_x]
-        imul ax, 2  ; Adjust speed here
-        idiv cx
-        mov [dx_ball], ax
-
-        mov ax, [mouse_y]
-        sub ax, [ball_y]
-        imul ax, 2  ; Adjust speed here
-        idiv cx
-        mov [dy_ball], ax
-
         ; Move ball
-        mov ax, [ball_x]
-        add ax, [dx_ball]
-        mov [next_ball_x], ax
-
-        mov ax, [ball_y]
-        add ax, [dy_ball]
-        mov [next_ball_y], ax
-
-        ; Check collision with edge pixels
-        ; Check the 8 surrounding pixels of the ball's edge
-        ; Assuming (next_ball_x, next_ball_y) is the center of the ball
-        mov si, [next_ball_x]
-        mov di, [next_ball_y]
-
-
-
+        ;X in AX
+        mov ax, player_x
+        add ax, si
+        ;Y in DX
+        mov dx, player_y
+        add dx, di
         
+        ;Check for Colision
+        call check_colision
+        cmp bh, 1d
+        je end_shoot
         
-        ; No collision, proceed to move the ball
-        mov [ball_x], [next_ball_x]
-        mov [ball_y], [next_ball_y]
-
+        ;erase current
+        call erase_current_ball
+        
         ; Draw ball at new position
-        ;mov bl, [current_ball]
-        ;mov cx, [ball_x]
-        ;mov dx, [ball_y]
-        ;call draw
+        mov location_x, ax
+        mov location_y, dx     
+        call draw_ball
 
         ; Small delay
         mov cx, 1000
-    delay_loop:
-        loop delay_loop
+        delay_loop:
+            loop delay_loop
+    jmp shoot_loop
 
-        jmp shoot_loop
+    end_shoot:
+        ret
 
+shoot endp
 
-    no_click:
-    pop dx
-    pop cx
-    pop bx
+; Input: AX = X coordinate, DX = Y coordinate
+; Output: BH = 1 if there is colision and 0 if not (bool func)
+check_colision proc uses di es si cx
+    ; Load the base segment for video memory
+    push ax
+    mov ax, 0A000h
+    mov es, ax
     pop ax
+    ; Calculate the offset: (Y * 320) + X
+    xor cx,cx
+    mov bx, dx        ; BX = Y
+    mov cl, 6d
+    shl bx, cl        ; BX = Y * 64
+    mov di, bx
+    mov cl, 2d         ; DI = BX
+    shl bx, cl        ; BX = Y * 256
+    add di, bx        ; DI = Y * 320 (64 + 256 = 320)
+    add di, ax        ; DI = Y * 320 + X
+    ;row check
+    mov bh, 1d ;setting the default value to be a colision.
+    mov cx, 12d
+    mov si, 0d
+    row_check:
+    push di
+    add di, si
+    mov bl, es:[di]   ; BL = color at (BX, DX)
+    pop di
+    cmp bl, background_color
+    jne end_colision_check
+    inc si
+    loop row_check
+
+    ; column check
+    mov cx, 12d
+    mov si, 0d
+    col_check:
+    push di
+    add di, si
+    mov bl, es:[di]   ; BL = color at (BX, DX)
+    pop di
+    cmp bl, background_color
+    jne end_shoot
+    push di
+    add di, si
+    add di, 11d
+    mov bl, es:[di]   ; BL = color at (BX, DX)
+    pop di
+    cmp bl, background_color
+    jne end_colision_check
+    add si,320d
+    loop col_check
+
+    mov bh, 0d ;if the proc got here then there is no colision
+    end_colision_check:
     ret
-check_mouse_and_shoot endp
+check_colision endp
+
+erase_current_ball proc uses ax es bx di si cx
+    ; Load the base segment for video memory
+    mov ax, 0A000h
+    mov es, ax
+    ; Calculate the offset: (Y * 320) + X
+    xor cx,cx
+    mov bx, player_y   ; BX = Y
+    mov cl, 6d
+    shl bx, cl         ; BX = Y * 64
+    mov di, bx         ; DI = BX
+    mov cl, 2d
+    shl bx, cl         ; BX = Y * 256
+    add di, bx         ; DI = Y * 320 (64 + 256 = 320)
+    add di, player_x   ; DI = Y * 320 + X
+    ;row check
+    mov cx, 12d
+    col_erase:
+        push cx
+        mov cx, 12d
+        mov si,0d
+        row_erase:
+            push di
+            add di, si
+            mov es:[di],background_color   ; BL = color at (BX, DX)
+            pop di
+            inc si
+        loop row_erase
+        pop cx
+        add di, 320d
+    loop col_erase
+    ret
+erase_current_ball endp
+
+
+;==================================================
+;This procedure handles the timer interrupt
+;==================================================
+new_Int1C proc far uses ax bx es si
+    ;down a line
+    int 80h                 ; Call DOS interrupt for timer
+    iret                    ; Return from interrupt
+new_Int1C endp
+
+;==================================================
+;This procedure changes the Interrupt Vector Table (IVT) for the timer
+;==================================================
+IVT_change proc uses ax es
+    mov ax, 0h              ; Set segment address to 0
+    mov es, ax
+    cli                     ; Clear interrupts
+
+    ; Copy old ISR 1C IP to free vector
+    mov ax, es:[1Ch*4]     ; Get old ISR IP
+    mov es:[80h*4], ax     ; Store IP at free vector
+    ; Copy old ISR 1C CS to free vector
+    mov ax, es:[1Ch*4+2]   ; Get old ISR CS
+    mov es:[80h*4+2], ax   ; Store CS at free vector
+
+    ; Copy IP of new_Int1C to IVT[1C]
+    mov ax, offset new_Int1C ; Get new ISR IP
+    mov es:[1Ch*4], ax     ; Set new IP in IVT
+    ; Copy CS of new_Int1C to IVT[1C]
+    mov ax, cs             ; Get new ISR CS
+    mov es:[1Ch*4+2], ax   ; Set new CS in IVT
+
+    sti                     ; Set interrupts
+    ret
+IVT_change endp
+
+;==================================================
+;This procedure restores the old Interrupt Vector Table (IVT) for the timer
+;==================================================
+IVT_return proc uses ax es
+    mov ax, 0h              ; Set segment address to 0
+    mov es, ax
+    cli                     ; Clear interrupts
+
+    ; Copy old ISR 1C IP to IVT[1C]
+    mov ax, es:[80h*4]     ; Get old ISR IP from free vector
+    mov es:[1Ch*4], ax     ; Set old IP in IVT
+    ; Copy old ISR 1C CS to IVT[1C]
+    mov ax, es:[80h*4+2]   ; Get old ISR CS from free vector
+    mov es:[1Ch*4+2], ax   ; Set old CS in IVT
+
+    sti                     ; Set interrupts
+    ret
+IVT_return endp
