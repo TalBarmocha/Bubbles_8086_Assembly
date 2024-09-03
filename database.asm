0cfa421a74920c5edf769d9d08427b4ac9ade5d1
shayrask <shayr9916@gmail.com>
2024-09-03 10:41:12 +0300

    #

--- database.asm
+++ database.asm
@@ -8,3 +8,26 @@ shift_loop:
 loop shift_loop
 ret
 shift_line_grid endp
+
+update_ball_back_grid proc uses ax bx cx si
+mov ax, player_y  
+sub ax, 5
+mov cx, 12
+div cx
+    
+mov cx, 13
+mul cx
+mov bx, ax  ; BX = nurmal location_y * 13
+    
+mov ax, player_x
+sub ax, 5
+mov cx, 12
+div cx      ; AX = nurmal location_x 
+    
+add bx, ax
+mov si, bx
+mov bl, current_ball
+mov bubble_grid[si], bl
+
+ret
+update_ball_back_grid endp
