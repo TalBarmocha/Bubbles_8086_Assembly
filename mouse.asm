42a651623672998e3c3f2b33206d8014594764ca
shayrask <shayr9916@gmail.com>
2024-09-16 16:59:57 +0300

    #

--- mouse.asm
+++ mouse.asm
@@ -160,28 +160,30 @@ shoot proc uses ax bx cx dx si di
     
     end_anim:
     ; Animation ends
-    push si
-    push di
-    ;convert to normal
-    mov cl, 8
-    shr si, cl
-    shr di, cl
 
-    cmp si, 2
+    push ax
+    push dx
+    mov ax, di
+    mov dx, si
+
+    cmp ah, 2
     jb no_baby_step_dx
-    sub ax, 1d
+    sub ah, 1d
     mov colli_stat, 0
     no_baby_step_dx:
-    cmp di, 2
+    cmp dh, 2
     jb no_baby_step_dy
     sub dh, 1d
     mov colli_stat, 0
     no_baby_step_dy:
+    mov di, ax
+    mov si, dx
     cmp colli_stat, 0
-    pop di
-    pop si
-    je no_wall_colli
+    pop dx
+    pop ax
+    je move_ball
 
+    
     pop dx
     pop ax
     mov ax, player_x
